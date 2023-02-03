4bc3e9f3f31e395d3854921ac98d2c427c3a669f
ErickChezOCs <nounga72@yahoo.fr>
2023-02-03 23:57:37 +0100

    install react-router-dom

---
+++ src/components/Accueil.js
@@ -0,0 +1,4 @@
+const Accueil = () => {
+    return <h1> Page d'accueil du site </h1>
+}
+export default Accueil;
---
+++ src/components/Produits.js
@@ -0,0 +1,4 @@
+const Produits = () => {
+    return <h1> Page des produits du site </h1>
+}
+export default Produits;
